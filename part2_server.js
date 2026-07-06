c8b29a8a1d48357ae0027d3a2f09a12c0d4a5871
PicksN1k <csjohnson2838@gmail.com>
2026-07-06 19:48:25 -0400

    modified:   part2_server.js

--- part2_server.js
+++ part2_server.js
@@ -16,6 +16,21 @@ const server = net.createServer((socket) => {
 		let result = "";
 		// fill the code
 
+		let command = input.trim().split(",");
+
+		if (command[0] === "lookupByCourseId") {
+			result = courseDB.lookupByCourseId(command[1]);
+		}
+		else if (command[0] === "lookupByCourseName") {
+			result = courseDB.lookupByCourseName(command[1]);
+		}
+		else if (command[0] === "getRandomCourse") {
+			result = courseDB.getRandomCourse();
+		}
+		else {
+			result = "Invalid Command";
+		}
+
 		socket.write(JSON.stringify(result));
 	});
 
