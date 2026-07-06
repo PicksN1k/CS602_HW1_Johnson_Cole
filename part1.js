5a76302b5c5dfee2c4009d209299d8f157f37f70
PicksN1k <csjohnson2838@gmail.com>
2026-07-06 19:46:31 -0400

    modified:   part1.js

--- part1.js
+++ part1.js
@@ -1,12 +1,17 @@
 import * as courseDB from './courseModule.js';
 
+console.log(courseDB.lookupByCourseId("CS 111"));
+
 console.log(courseDB.lookupByCourseId("CS 602"));
+
 console.log(courseDB.lookupByCourseId("^CS 2"));
+
 console.log(courseDB.lookupByCourseId("6.2$"));
 
 console.log(courseDB.lookupByCourseName("Web"));
 
 console.log(courseDB.getRandomCourse());
+
 console.log(courseDB.getRandomCourse());
 
 
